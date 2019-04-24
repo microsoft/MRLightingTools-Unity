44eeeb7f5f5bdacd464fe7ae451ca1f56a6e7f20
Nick Klingensmith <programmerpichu@gmail.com>
2019-04-24 12:03:39 -0700

    Additive shadow tools.

---
+++ Assets/MixedRealityToolkit.LightingTools/Shaders/ShadowARTransparent.shader
@@ -0,0 +1,51 @@
+Shader "Mixed Reality Toolkit/Shadow AR Transparent" {
+	Properties {
+		_Color ("Main Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags {"Queue" = "Geometry" "RenderType" = "Opaque"}
+
+		Pass {
+			Tags {"LightMode" = "ForwardBase"}
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#pragma multi_compile_fwdbase
+				#pragma fragmentoption ARB_fog_exp2
+				#pragma fragmentoption ARB_precision_hint_fastest
+				
+				#include "UnityCG.cginc"
+				#include "AutoLight.cginc"
+				
+				struct v2f
+				{
+					float4 pos : SV_POSITION;
+					float3 normal : NORMAL;
+					LIGHTING_COORDS(1,2)
+				};
+
+				float4 _Color;
+
+				v2f vert (appdata_tan v)
+				{
+					v2f o;
+					
+					o.pos = UnityObjectToClipPos(v.vertex);
+					o.normal = UnityObjectToWorldNormal(v.normal);
+					TRANSFER_VERTEX_TO_FRAGMENT(o);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : COLOR
+				{
+					//fixed atten = LIGHT_ATTENUATION(i);	// Light attenuation + shadows.
+					float facing = saturate(-dot(i.normal, _WorldSpaceLightPos0.xyz)*1000);
+					fixed atten = SHADOW_ATTENUATION(i); // Shadows ONLY.
+					clip((0.5-atten) - facing);
+					return _Color;// tex2D(_MainTex, i.uv) * (lerp(UNITY_LIGHTMODEL_AMBIENT, fixed4(1, 1, 1, 1), atten)) * _Color;
+				}
+			ENDCG
+		}
+	}
+	FallBack "VertexLit"
+}
---
+++ Assets/MixedRealityToolkit.LightingTools/ShadowsAdditive.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ShadowsAdditive: MonoBehaviour {
+	[SerializeField, Range(0, 1)] float _brightness = 0.1f;
+
+    MeshFilter   _filter;
+	MeshRenderer _renderer;
+    int          _brightnessId;
+
+    public float Brightness {
+        get { return _brightness; }
+        set {
+            _brightness = value;
+            if (_renderer != null)
+                _renderer.sharedMaterial.SetFloat(_brightnessId, _brightness);
+        }
+    }
+
+    void Start ()
+    {
+		_filter       = GetComponent<MeshFilter>();
+		_renderer     = GetComponent<MeshRenderer>();
+        _brightnessId = Shader.PropertyToID("_Brightness");
+
+        if (_filter.sharedMesh == null)
+		    _filter.sharedMesh = new Mesh();
+        Mesh mesh = _filter.sharedMesh;
+        CreatePlane(ref mesh);
+        
+        _renderer.sharedMaterial = new Material(Shader.Find("Hidden/Shadow Screen Vignette"));
+        Brightness = _brightness;
+    }
+    
+	Mesh CreatePlane(ref Mesh m)
+    {
+		m.vertices  = new Vector3[] { new Vector3(-1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, -1, 0), new Vector3(-1, -1, 0) };
+		m.triangles = new int[] { 0,1,2,  0,2,3 };
+		m.bounds    = new Bounds(Vector3.zero, new Vector3(float.MaxValue, float.MaxValue, float.MaxValue));
+		return m;
+	}
+
+	private void OnValidate()
+	{
+		Brightness = _brightness;
+    }
+}
