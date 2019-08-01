298c16773922d90700bd4f5181d99628b4840622
Nick Klingensmith <programmerpichu@gmail.com>
2019-08-01 14:04:03 -0700

    Removing additive shadows from dev branch

--- Assets/MixedRealityToolkit.LightingTools/Scripts/Editor/LightingToolsMenus.cs
+++ Assets/MixedRealityToolkit.LightingTools/Scripts/Editor/LightingToolsMenus.cs
@@ -14,14 +14,5 @@ namespace Microsoft.MixedReality.Toolkit.LightingTools {
 			GameObject go = new GameObject("LightCapture", typeof(LightCapture));
 			EditorGUIUtility.PingObject(go);
 		}
-
-        /// <summary> Creates a starter Light Capture object in the scene. </summary>
-		[MenuItem("Mixed Reality Toolkit/Lighting Tools/Create Additive Shadow Object", priority = 1)]
-        private static void CreateASObject()
-        {
-            GameObject go = new GameObject("AdditiveShadows", typeof(ShadowsAdditive));
-            EditorGUIUtility.PingObject(go);
-            Debug.LogWarning("For additive shadows to work, you must have shadows enabled for your quality settings! You must also have a shadow catching surface with the 'MRTK/Shadow AR Transparent' shader on it, use the Windows Mixed Reality package and the Spatial Mapping Renderer visuals, or the MRTK's spatial mapping mesh.");
-        }
-    }
+	}
 }
